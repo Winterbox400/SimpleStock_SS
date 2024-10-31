f5a3fedfda3684b29ea949bbcd7496dc60c54abf
Winterbox400 <sandigojustin@gmail.com>
2024-10-30 19:21:39 -0600

    Inicio del desarrollo

---
+++ APP_SimpleStock_SS/SimpleStock_SS/DAO/DBConnection.vb
@@ -0,0 +1,81 @@
+﻿'Importacion de Clases del Sistema
+Imports System.Data.SqlClient 'Clase SQL
+
+Public Class DBConnection
+
+    Private connectionString As String 'Añadiremos la informacion de conexion con SqlServert
+    Private Servidor As String = "WINTERBOX400\SQL2022"
+    Private NombreDB As String = "BD_SimpleStock_SS"
+    Private UserDB As String = "sa"
+    Private PasswordDB As String = "1234"
+    Private connection As SqlConnection 'Creamos la variable de tipo SqlConnetion para pasarle el string anterior
+
+    ' Metodo Constructor que establece la cadena de conexión
+    Public Sub New()
+        connectionString ='Le pasamos los datos para la conexión
+            "Data Source=" + Servidor +
+            ";Initial Catalog=" + NombreDB +
+            ";User ID=" + UserDB +
+            ";Password=" + PasswordDB
+        connection = New SqlConnection(connectionString)
+    End Sub
+
+
+
+    ' Método para abrir la conexión
+    Public Function OpenConnection() As Boolean
+        Try
+            If connection.State = ConnectionState.Closed Then 'Evalua si el estado de la conexión esta cerrado
+                connection.Open() 'Abre la conexión
+            End If
+            Return True
+        Catch ex As Exception 'En caso de fallar ejecuta un mensaje de error
+            MessageBox.Show("Error al abrir la conexión: " & ex.Message)
+            Return False
+        End Try
+    End Function
+
+
+
+    ' Método para cerrar la conexión
+    Public Sub CloseConnection()
+        If connection.State = ConnectionState.Open Then 'Evalua si el estado de la conexión esta abierto
+            connection.Close() 'Cierra la conexión
+        End If
+    End Sub
+
+    ' Método para ejecutar consultas de tipo SELECT
+    Public Function ExecuteQuery(query As String) As DataTable
+        Dim dataTable As New DataTable()
+        Try
+            If OpenConnection() Then
+                Using command As New SqlCommand(query, connection)
+                    Using adapter As New SqlDataAdapter(command)
+                        adapter.Fill(dataTable)
+                    End Using
+                End Using
+                CloseConnection()
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error al ejecutar consulta: " & ex.Message)
+        End Try
+        Return dataTable
+    End Function
+
+    ' Método para ejecutar consultas de tipo INSERT, UPDATE, DELETE
+    Public Function ExecuteNonQuery(query As String) As Integer
+        Dim rowsAffected As Integer = 0
+        Try
+            If OpenConnection() Then
+                Using command As New SqlCommand(query, connection)
+                    rowsAffected = command.ExecuteNonQuery()
+                End Using
+                CloseConnection()
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error al ejecutar comando: " & ex.Message)
+        End Try
+        Return rowsAffected
+    End Function
+
+End Class
---
+++ BaseDeDatos/CreacionBD_Tablas.sql
@@ -0,0 +1,149 @@
+CREATE DATABASE BD_SimpleStock_SS
+GO
+
+USE BD_SimpleStock_SS
+GO
+
+CREATE TABLE Categorias(
+IdCategoria INT PRIMARY KEY IDENTITY,
+Nombre VARCHAR(20) NOT NULL,
+Descripcion VARCHAR(250) NOT NULL,
+Activo BIT NOT NULL,--InicioPistasAuditoria
+IdUsuarioRegistro INT NOT NULL,
+FechaRegistro DATETIME NOT NULL,
+IdUsuarioActualiza INT NULL,
+FechaActualizacion DATETIME NULL--FinPistasAuditoria
+)
+GO
+
+CREATE TABLE Marcas(
+IdMarca INT PRIMARY KEY IDENTITY,
+Nombre VARCHAR(20) NOT NULL,
+Descripcion VARCHAR(250) NOT NULL,
+Activo BIT NOT NULL,--InicioPistasAuditoria
+IdUsuarioRegistro INT NOT NULL,
+FechaRegistro DATETIME NOT NULL,
+IdUsuarioActualiza INT NULL,
+FechaActualizacion DATETIME NULL--FinPistasAuditoria
+)
+GO
+
+CREATE TABLE Proveedores(
+IdProveedor INT PRIMARY KEY IDENTITY,
+Nombre VARCHAR(20) NOT NULL,
+Telefono VARCHAR(20) NOT NULL,
+Correo VARCHAR(50) NOT NULL,
+Activo BIT NOT NULL,--InicioPistasAuditoria
+IdUsuarioRegistro INT NOT NULL,
+FechaRegistro DATETIME NOT NULL,
+IdUsuarioActualiza INT NULL,
+FechaActualizacion DATETIME NULL--FinPistasAuditoria
+)
+GO
+
+CREATE TABLE Productos(
+IdProducto INT PRIMARY KEY IDENTITY,
+Nombre VARCHAR(50) NOT NULL,
+Descripcion VARCHAR(250) NOT NULL,
+Almacen VARCHAR(50) NOT NULL,
+Ubicacion VARCHAR(50) NOT NULL,
+IdCategoria INT FOREIGN KEY REFERENCES Categorias(IdCategoria),
+IdMarca INT FOREIGN KEY REFERENCES Marcas(IdMarca),
+IdProveedore INT FOREIGN KEY REFERENCES Proveedores(IdProveedor),
+Activo BIT NOT NULL,--InicioPistasAuditoria
+IdUsuarioRegistro INT NOT NULL,
+FechaRegistro DATETIME NOT NULL,
+IdUsuarioActualiza INT NULL,
+FechaActualizacion DATETIME NULL--FinPistasAuditoria
+)
+GO
+
+CREATE TABLE TipoEntradaSalida(
+IdTipo INT PRIMARY KEY IDENTITY,
+Tipo VARCHAR(20) NOT NULL,
+Descripcion VARCHAR(250) NOT NULL,
+Activo BIT NOT NULL,--InicioPistasAuditoria
+IdUsuarioRegistro INT NOT NULL,
+FechaRegistro DATETIME NOT NULL,
+IdUsuarioActualiza INT NULL,
+FechaActualizacion DATETIME NULL--FinPistasAuditoria
+)
+GO
+
+CREATE TABLE RegistroEntradas(
+IdRegistroEntrada INT PRIMARY KEY IDENTITY,
+FechaEntrada DATETIME NOT NULL,
+IdTipo INT FOREIGN KEY REFERENCES TipoEntradaSalida(IdTipo),
+Descripcion VARCHAR(250) NOT NULL,
+Activo BIT NOT NULL,--InicioPistasAuditoria
+IdUsuarioRegistro INT NOT NULL,
+FechaRegistro DATETIME NOT NULL,
+IdUsuarioActualiza INT NULL,
+FechaActualizacion DATETIME NULL--FinPistasAuditoria
+)
+GO
+
+CREATE TABLE RegistroSalidas(
+IdRegistroSalida INT PRIMARY KEY IDENTITY,
+FechaSalida DATETIME NOT NULL,
+IdTipo INT FOREIGN KEY REFERENCES TipoEntradaSalida(IdTipo),
+Descripcion VARCHAR(250) NOT NULL,
+Activo BIT NOT NULL,--InicioPistasAuditoria
+IdUsuarioRegistro INT NOT NULL,
+FechaRegistro DATETIME NOT NULL,
+IdUsuarioActualiza INT NULL,
+FechaActualizacion DATETIME NULL--FinPistasAuditoria
+)
+GO
+
+CREATE TABLE DetalleRegistroEntrada(
+IdDetalleRegistroEntrada INT PRIMARY KEY IDENTITY,
+IdRegistroEntrada INT FOREIGN KEY REFERENCES RegistroEntradas(IdRegistroEntrada),
+IdProducto INT FOREIGN KEY REFERENCES Productos(IdProducto),
+Cantidad INT NOT NULL,
+PrecioCompra DECIMAL(20,2) NOT NULL,
+Activo BIT NOT NULL,--InicioPistasAuditoria
+IdUsuarioRegistro INT NOT NULL,
+FechaRegistro DATETIME NOT NULL,
+IdUsuarioActualiza INT NULL,
+FechaActualizacion DATETIME NULL--FinPistasAuditoria
+)
+GO
+
+CREATE TABLE DetalleRegistroSalida(
+IdDetalleRegistroSalida INT PRIMARY KEY IDENTITY,
+IdProducto INT FOREIGN KEY REFERENCES Productos(IdProducto),
+Cantidad INT NOT NULL,
+Activo BIT NOT NULL,--InicioPistasAuditoria
+IdUsuarioRegistro INT NOT NULL,
+FechaRegistro DATETIME NOT NULL,
+IdUsuarioActualiza INT NULL,
+FechaActualizacion DATETIME NULL--FinPistasAuditoria
+)
+GO
+
+CREATE TABLE MovimientosInventario(
+IdMovimientoInventario INT PRIMARY KEY IDENTITY,
+TipoMovimiento BIT NOT NULL, -- 1 será entrada, 0 será salida
+IdDetalleRegistroEntrada INT FOREIGN KEY REFERENCES DetalleRegistroEntrada(IdDetalleRegistroEntrada) NULL,
+IdDetalleRegistroSalida INT FOREIGN KEY REFERENCES DetalleRegistroSalida(IdDetalleRegistroSalida) NULL,
+Activo BIT NOT NULL,--InicioPistasAuditoria
+IdUsuarioRegistro INT NOT NULL,
+FechaRegistro DATETIME NOT NULL,
+IdUsuarioActualiza INT NULL,
+FechaActualizacion DATETIME NULL--FinPistasAuditoria
+)
+GO
+
+CREATE TABLE StockInventario(
+IdStrockInventario INT PRIMARY KEY IDENTITY,
+IdMovimientoInventario INT FOREIGN KEY REFERENCES MovimientosInventario(IdMovimientoInventario),
+CantidadActual INT NOT NULL,
+StockMinimmo INT NOT NULL,
+Activo BIT NOT NULL,--InicioPistasAuditoria
+IdUsuarioRegistro INT NOT NULL,
+FechaRegistro DATETIME NOT NULL,
+IdUsuarioActualiza INT NULL,
+FechaActualizacion DATETIME NULL--FinPistasAuditoria
+)
+GO
